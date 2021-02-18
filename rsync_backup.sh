fa6391270cded2b9ca92ae738332804f2f0a96ef
João Tagaio <joao.tagaio@tecnico.ulisboa.pt>
2021-02-18 00:31:45 +0000

    Correct a bug in rsync script

--- rsync_backup.sh
+++ rsync_backup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 #This script backups the whole system using rsync
-sudo rsync -aAXHv -M--fake-super --numeric-ids -e "ssh -p 42033 -i /home/jptagaio/.ssh/id_ed25519" --progress --delete --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","swapfile","lost+found",".cache","Transferências","VirtualBox VMs","Dropbox","MEOCloud",".ecryptfs" / systemAdmin@192.168.1.68:/share/Backup_Arch/
+sudo rsync -aAXHv -M--fake-super --numeric-ids -e "ssh -p 42033 -i /home/jptagaio/.ssh/id_ed25519" --progress --delete --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","swapfile","lost+found",".cache","Transferências","VirtualBox VMs","Dropbox","MEOCloud",".ecryptfs"} / systemAdmin@192.168.1.68:/share/Backup_Arch/
 
